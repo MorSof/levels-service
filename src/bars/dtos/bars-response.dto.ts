609a76958b0be206ad0a4af007725c1fa915c2b6
MorSof <morsof48@gmail.com>
2023-05-27 14:10:51 +0300

    fix(bars-response):add childeren

--- src/bars/dtos/bars-response.dto.ts
+++ src/bars/dtos/bars-response.dto.ts
@@ -1,6 +1,30 @@
 import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import { ResourceResponseDto } from './resource-response.dto';
 
+/**
+ Added the MilestoneRequestDto class just for swagger documentation,
+ because it didn't allow me to use the BaseBarsRequestDto type in the ApiModelProperty of the milestones,
+ because it's not yet declared.
+ Also, this object doesn't include the MilestoneRequestDto too, for the same reason.
+ Take a look at the BaseBarsRequestDto and see that the type of milestones is BaseBarsRequestDto and not MilestoneRequestDto,
+ so a milestone is also a bar that contains milestones
+ **/
+export class MilestoneResponseDto {
+  @ApiModelProperty({
+    type: 'number',
+    description: 'The max value to fill the milestone bar',
+    default: 50,
+  })
+  maxValue: number;
+
+  @ApiModelProperty({
+    isArray: true,
+    type: ResourceResponseDto,
+    description: 'An array of rewards for the bar',
+  })
+  rewards?: ResourceResponseDto[];
+}
+
 export class BarsResponseDto {
   @ApiModelProperty({
     type: 'number',
@@ -27,7 +51,7 @@ export class BarsResponseDto {
 
   @ApiModelProperty({
     isArray: true,
-    type: BarsResponseDto,
+    type: MilestoneResponseDto,
     description: 'An array of milestones in the bar',
   })
   milestones?: BarsResponseDto[];
--- src/bars/dtos/resource-response.dto.ts
+++ src/bars/dtos/resource-response.dto.ts
@@ -1,5 +1,60 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+/**
+ Added the InnerResourceResponse class just for swagger documentation,
+ because it didn't allow me to use the BaseResourceRequestDto type in the ApiProperty of the inner resources,
+ because it's not yet declared.
+ Also, this object doesn't include the InnerResourceRequestDto too, for the same reason.
+ Take a look at the ResourceResponseDto and see that the type of inner resources is ResourceResponseDto and not InnerResourceResponse,
+ so an inner resource is also a resource that contains inner resources
+ **/
+export class InnerResourceResponse {
+  readonly ownerId?: string;
+
+  readonly ownerType?: string;
+
+  readonly groupId?: string;
+
+  @ApiProperty({})
+  readonly type: string;
+
+  @ApiProperty({})
+  readonly name: string;
+
+  @ApiProperty({
+    type: 'number',
+    nullable: true,
+    default: null,
+  })
+  readonly amount?: number;
+
+  @ApiProperty({
+    type: 'number',
+    nullable: true,
+    minimum: 0,
+    maximum: 1,
+    default: 1,
+  })
+  readonly receivingProbability: number;
+
+  @ApiProperty({
+    type: 'number',
+    nullable: true,
+    minimum: 0,
+    maximum: 1,
+    default: 0.5,
+  })
+  readonly rarenessProbability: number;
+
+  @ApiProperty({
+    type: 'object',
+    additionalProperties: true,
+    required: false,
+    nullable: true,
+  })
+  readonly extraArgs: Record<string, any>;
+}
+
 export class ResourceResponseDto {
   @ApiProperty()
   id: number;
@@ -31,7 +86,7 @@ export class ResourceResponseDto {
 
   @ApiProperty({
     isArray: true,
-    type: ResourceResponseDto,
+    type: InnerResourceResponse,
   })
   resources?: ResourceResponseDto[];
 
