88779c632b95d190f56f7d581bbcc8c4c42987c3
MorSof <morsof48@gmail.com>
2023-08-26 16:25:01 +0300

    renaming

--- src/bars/providers/bars-microservice-provider/bars-microservice-provider.service.ts
+++ src/bars/providers/bars-microservice-provider/bars-microservice-provider.service.ts
@@ -12,21 +12,21 @@ import { BarsApi } from '@morsof/bars-service-api';
 @Injectable()
 export class BarsMicroserviceProvider extends BarsProvider {
   constructor(
-    @Inject('BarsApi') private readonly resourcesApi: BarsApi,
+    @Inject('BarsApi') private readonly barsApi: BarsApi,
     private readonly barsDtoConverter: BarsDtoConverter,
   ) {
     super();
   }
 
   async fetchBarsByName(name: string): Promise<Bar[]> {
-    return (await this.resourcesApi.v1BarsGet(name)).map((dto) => {
+    return (await this.barsApi.v1BarsGet(name)).map((dto) => {
       return this.barsDtoConverter.toModel(dto);
     });
   }
 
   async createBar(bar: Bar): Promise<Bar> {
     const body = this.barsDtoConverter.toDto(bar);
-    const barResponseDto = await this.resourcesApi
+    const barResponseDto = await this.barsApi
       .v1BarsPost(body)
       .catch((error: { code: number }) => {
         if (
